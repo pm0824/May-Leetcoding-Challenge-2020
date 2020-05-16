f65998b94e436db68482689d694fa3adb646181e
Prachi Mane <46194962+pm0824@users.noreply.github.com>
2020-05-16 12:01:59 +0530

    Day 15

---
+++ Maximum_Sum_Circular_Subarray.py
@@ -0,0 +1,62 @@
+'''
+Given a circular array C of integers represented by A, find the maximum possible sum of a non-empty subarray of C.
+Here, a circular array means the end of the array connects to the beginning of the array.  
+(Formally, C[i] = A[i] when 0 <= i < A.length, and C[i+A.length] = C[i] when i >= 0.)
+Also, a subarray may only include each element of the fixed buffer A at most once.  
+(Formally, for a subarray C[i], C[i+1], ..., C[j], there does not exist i <= k1, k2 <= j with k1 % A.length = k2 % A.length.)
+
+Example 1:
+Input: [1,-2,3,-2]
+Output: 3
+Explanation: Subarray [3] has maximum sum 3
+
+Example 2:
+Input: [5,-3,5]
+Output: 10
+Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10
+
+Example 3:
+Input: [3,-1,2,-1]
+Output: 4
+Explanation: Subarray [2,-1,3] has maximum sum 2 + (-1) + 3 = 4
+
+Example 4:
+Input: [3,-2,2,-3]
+Output: 3
+Explanation: Subarray [3] and [3,-2,2] both have maximum sum 3
+
+Example 5:
+Input: [-2,-3,-1]
+Output: -1
+Explanation: Subarray [-1] has maximum sum -1
+ 
+Note:
+-30000 <= A[i] <= 30000
+1 <= A.length <= 30000
+'''
+'''
+Solution: Using Kadane's Algorithm
+
+case 1: If the max sum subarray is present in the middle then we can use normal kadane's algorithm 
+case 2: If it is at the ends of array i.e. it wraps the array then we have to find 
+        Total_sum - Min_sum_subarray
+Corner case :
+If all numbers are negative, maxSum = max(A) and minSum = sum(A).
+In this case, max(maxSum, total - minSum) = 0, which means the sum of an empty subarray.
+According to the deacription, We need to return the max(A), instead of sum of am empty subarray.
+So we return the maxSum to handle this corner case.
+'''
+
+class Solution:
+    def maxSubarraySumCircular(self, A: List[int]) -> int:
+        tot, max_sum, cur_max, min_sum, cur_min = 0, -float("inf"), 0, float("inf"), 0
+        for a in A:
+            cur_max = max(cur_max+a, a)
+            max_sum = max(max_sum, cur_max)
+            cur_min = min(cur_min+a, a)
+            min_sum = min(min_sum, cur_min)
+            tot += a
+        return max(max_sum, tot-min_sum) if max_sum >= 0 else max_sum
+        
+        
+        
